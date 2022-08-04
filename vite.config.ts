7eee6e28389e24bb9f19c5c5a11aa38eeb1e5bc8
Diogolima-creator <dbznetudo@gmail.com>
2022-08-04 14:39:54 -0400

    xx

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build:{
-    rollupOptions:{
-      external: [
-        '@fortawesome',
-        path.resolve( __dirname, 'src/components/menuSubject/index' ),
-        /node_modules/
-      ]
-    }
-  }
+  plugins: [react()]
 })
 
