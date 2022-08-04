98b318a6e8ac190b4abee59bb8b3e0c1dbce505d
Diogolima-creator <dbznetudo@gmail.com>
2022-08-04 14:38:33 -0400

    xx

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,5 @@ declare module '*.jpeg';
 declare module '*.mp4';
 declare module '*.jpg';
 declare module '*.png';
-declare module './styles';
-declare module '../../services/Grades';
 declare module '@fortawesome/free-solid-svg-icons';
 declare module '@fortawesome/react-fontawesome';
