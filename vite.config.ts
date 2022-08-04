774a8a52b9b43347f5796ab8b3cd8e676e9da884
Diogolima-creator <dbznetudo@gmail.com>
2022-08-04 13:46:06 -0400

    hmm

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions:{
-      external:[
-        /^@fortawesome:.*/,
-      ]
-    }
-  }
+  plugins: [react(), nodeResolve()],
 })
+
